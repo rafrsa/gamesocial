b0e975b584b406d413346544ebb6333809cdf604
Rafael Frecceiro Santos <rafa.frecceiro@gmail.com>
2018-10-16 17:35:21 -0300

    Alteração do layout dos cards da timeline

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,6 +1,20 @@
 @if($timeline)
     @foreach($timeline as $key => $value)
-        <h1>{{$value->mensagem}}</h1>
+{{--        <h1>{{$value->mensagem}}</h1>--}}
+        <div class="col-md-8  col-xl-8" style="margin: 0px auto">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Panel with custom buttons</h3>
+                    <div class="card-options">
+                        <a href="#" class="btn btn-primary btn-sm">Action 1</a>
+                        <a href="#" class="btn btn-secondary btn-sm ml-2">Action 2</a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    {{$value->mensagem}}
+                </div>
+            </div>
+        </div>
     @endforeach
 @else
     <h1>Sem atividade para exibir!</h1>
